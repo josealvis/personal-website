8c86b459ff7b8f2c58e6360339f250e7d6803ce2
Jose Alvis <jalvis1491@gmail.com>
2021-02-22 10:00:26 -0500

    remove unitest

--- src/components/Home.spec.js
+++ src/components/Home.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import Home from './Home.vue'
 
-it('renders correctly', () => {
-    const wrapper = mount(Home)
-    expect(wrapper.element).toMatchSnapshot();
-  });
+// it('renders correctly', () => {
+//     const wrapper = mount(Home)
+//     expect(wrapper.element).toMatchSnapshot();
+//   });
